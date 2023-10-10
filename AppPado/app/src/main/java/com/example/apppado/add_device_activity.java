bb16c6f2d5f663ce08f1fb0d3fb180856fb7b493
giovanebego1 <giovanebego@outlook.com>
2023-10-10 20:17:19 -0300

    App first commit
    On this app commit we have the most part of software front end developed and some essential parts of backend that we need to run the first screams of  the app

---
+++ AppPado/app/src/main/java/com/example/apppado/add_device_activity.java
@@ -0,0 +1,13 @@
+package com.example.apppado;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class add_device_activity extends Activity {
+	ImageView teste;
+}
+	
+	
