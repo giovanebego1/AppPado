3eecd2f8873dacecbafc7c69b4bc80438c52a5f6
HBosc <heitorbosc@gmail.com>
2023-10-10 21:12:18 -0300

    register.db file creation
    cria o arquivo register.db

--- AppPado/app/src/main/java/com/example/apppado/DBHelper.java
+++ AppPado/app/src/main/java/com/example/apppado/DBHelper.java
@@ -1,5 +1,5 @@
 package com.example.apppado;
-
+/** 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -57,3 +57,69 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
 }
+**/
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String TAG = "DBHelper";
+    private static final String DB_NAME = "register.db";
+    private static final int DB_VERSION = 1;
+    private final Context context;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // No need to create the database structure here if you have it pre-populated.
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Handle database upgrades if needed.
+    }
+
+    public void copyDatabase() throws IOException {
+        InputStream inputStream = context.getAssets().open(DB_NAME);
+        String outFileName = context.getDatabasePath(DB_NAME).getPath();
+        OutputStream outputStream = new FileOutputStream(outFileName);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+    }
+
+    public SQLiteDatabase openDatabase() {
+        String dbPath = context.getDatabasePath(DB_NAME).getPath();
+        return SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+}
+DBHelper dbHelper = new DBHelper(this);
+
+try {
+    dbHelper.copyDatabase();
+} catch (IOException e) {
+    Log.e("DBHelper", "Erro ao copiar o banco de dados");
+}
+
+// Agora você pode chamar openDatabase() para obter uma referência ao banco de dados
+SQLiteDatabase db = dbHelper.openDatabase();
+
+// Use o banco de dados normalmente
+// Exemplo: dbHelper.insertData("usuario123", "senha123", "Nome Completo", "123456789");
